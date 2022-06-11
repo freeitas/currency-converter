25ade1cdb981085efea54c629111fc258841f172
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-06-11 09:53:15 -0300

    feat: currency and common constructor

--- src/money.ts
+++ src/money.ts
@@ -1,12 +1,18 @@
 export abstract class Money {
   public amount: number
+  public _currency: string
 
-  static dollar (amount: number): Dollar {
-    return new Dollar(amount)
+  constructor (amount: number, currency: string) {
+    this.amount = amount
+    this._currency = currency
   }
 
-  static euro (amount: number): Euro {
-    return new Euro(amount)
+  static dollar (amount: number): Money {
+    return new Dollar(amount, 'USD')
+  }
+
+  static euro (amount: number): Money {
+    return new Euro(amount, 'EUR')
   }
 
   equals (other: Money): boolean {
@@ -17,26 +23,19 @@ export abstract class Money {
   }
 
   abstract times(multiplier: number): Money
+  currency (): string {
+    return this._currency
+  }
 }
 
 export class Dollar extends Money {
-  constructor (amount: number) {
-    super()
-	  super.amount = amount
-  }
-
   times (multiplier: number): Dollar {
-	  return new Dollar(this.amount * multiplier)
+	  return Money.dollar(this.amount * multiplier)
   }
 }
 
 export class Euro extends Money {
-  constructor (amount: number) {
-    super()
-	  super.amount = amount
-  }
-
   times (multiplier: number): Euro {
-	  return new Euro(this.amount * multiplier)
+	  return Money.euro(this.amount * multiplier)
   }
 }
