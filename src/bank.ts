450c60366efda6b413c19ced95fab75eb3e919e2
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-06-15 22:06:25 -0300

    test: reduce from a sum and from a money

--- src/bank.ts
+++ src/bank.ts
@@ -2,7 +2,7 @@ import { Expression } from './expression'
 import { Money } from './money'
 
 export class Bank {
-  reduce (souce: Expression, to: string): Money {
-    return Money.dollar(10)
+  reduce (source: Expression, to: string): Money {
+    return source.reduce(to)
   }
 }
